b27197caf55586eeb07fac5abc2475963ed86a21
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2017-05-03 21:07:17 +0200

    Indentation and other stuff.

--- js/countdown.js
+++ js/countdown.js
@@ -1,42 +1,48 @@
-﻿function CountdownTimer(elm,tl,mes){
- this.initialize.apply(this,arguments);
+﻿function CountdownTimer(elm, tl, mes) {
+    this.initialize.apply(this, arguments);
 }
-CountdownTimer.prototype={
- initialize:function(elm,tl,mes) {
-  this.elem = document.getElementById(elm);
-  this.tl = tl;
-  this.mes = mes;
- },countDown:function(){
-  var timer='';
-  var today=new Date();
-  var day=Math.floor((this.tl-today)/(24*60*60*1000));
-  var hour=Math.floor(((this.tl-today)%(24*60*60*1000))/(60*60*1000));
-  var min=Math.floor(((this.tl-today)%(24*60*60*1000))/(60*1000))%60;
-  var sec=Math.floor(((this.tl-today)%(24*60*60*1000))/1000)%60%60;
-  var me=this;
+CountdownTimer.prototype = {
+    initialize: function (elm, tl, mes) {
+        this.elem = document.getElementById(elm);
+        this.tl = tl;
+        this.mes = mes;
+    }, countDown: function () {
+        var timer = '';
+        var today = new Date();
+        var day = Math.floor((this.tl - today) / (24 * 60 * 60 * 1000));
+        var hour = Math.floor(((this.tl - today) % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+        var min = Math.floor(((this.tl - today) % (24 * 60 * 60 * 1000)) / (60 * 1000)) % 60;
+        var sec = Math.floor(((this.tl - today) % (24 * 60 * 60 * 1000)) / 1000) % 60 % 60;
+        var me = this;
 
-  if( ( this.tl - today ) > 0 ){
-   timer += '<span class="number-wrapper"><div class="line"></div><div class="caption">DAYS</div><span class="number day">'+day+'</span></span>';
-   timer += '<span class="number-wrapper"><div class="line"></div><div class="caption">HOURS</div><span class="number hour">'+hour+'</span></span>';
-   timer += '<span class="number-wrapper"><div class="line"></div><div class="caption">MINS</div><span class="number min">'+this.addZero(min)+'</span></span><span class="number-wrapper"><div class="line"></div><div class="caption">SECS</div><span class="number sec">'+this.addZero(sec)+'</span></span>';
-   this.elem.innerHTML = timer;
-   tid = setTimeout( function(){me.countDown();},10 );
-  }else{
-   this.elem.innerHTML = this.mes;
-   return;
-  }
- },addZero:function(num){ return ('0'+num).slice(-2); }
+        if (( this.tl - today ) > 0) {
+            timer += '<span class="number-wrapper"><div class="line"></div><div class="caption">DAYS</div><span class="number day">' + this.addZero(day) + '</span></span>';
+            timer += '<span class="number-wrapper"><div class="line"></div><div class="caption">HOURS</div><span class="number hour">' + this.addZero(hour) + '</span></span>';
+            timer += '<span class="number-wrapper"><div class="line"></div><div class="caption">MINS</div><span class="number min">' + this.addZero(min) + '</span></span>';
+            timer += '<span class="number-wrapper"><div class="line"></div><div class="caption">SECS</div><span class="number sec">' + this.addZero(sec) + '</span></span>';
+            this.elem.innerHTML = timer;
+            tid = setTimeout(function () {
+                me.countDown();
+            }, 10);
+        } else {
+            this.elem.innerHTML = this.mes;
+            return;
+        }
+    }, addZero: function (num) {
+        return ('0' + num).slice(-2);
+    }
+};
+function CDT(mytime) {
+
+    // Set countdown limit
+    //var tl = new Date('2016/09/21 17:00:00');
+    var tl = new Date(mytime);
+
+    // You can add time's up message here
+    var timer = new CountdownTimer('CDT', tl, '<span class="number-wrapper"><div class="line"></div><span id="clock_string" class="number end">The meeting is on :)</span></span>');
+    timer.countDown();
 }
-function CDT(mytime){
 
- // Set countdown limit
- //var tl = new Date('2016/09/21 17:00:00');
-var tl = new Date(mytime);
-
- // You can add time's up message here
- var timer = new CountdownTimer('CDT',tl,'<span class="number-wrapper"><div class="line"></div><span id="clock_string" class="number end">The meeting is on :)</span></span>');
- timer.countDown();
-}
-window.onload=function(){
- //CDT();
+window.onload = function () {
+    //CDT();
 }
